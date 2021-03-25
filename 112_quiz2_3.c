753c5a920e93e6c56b1c09831d38fb8937d3eafe
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-25 17:31:10 +0700

    Palindrome

--- 112_quiz2_3.c
+++ 112_quiz2_3.c
@@ -3,9 +3,10 @@ int main()
 {
     int n,i,j,count=0;
     scanf("%d" ,&n);
+    int palindrome[]={1,2,3,4,5,6,7,8,9,11,22,33,44,55,66,77,88,99};
     for(i=1;i<=n;i++)
     {
-        if(i==n)
+        if(i==palindrome[i-1])
         {
             count++;
         }
