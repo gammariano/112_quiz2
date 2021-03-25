97a557b49eb2019881e97a813dd793ad170156d5
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-25 18:20:27 +0700

    Silk Texture

--- 112_quiz2_2.c
+++ 112_quiz2_2.c
@@ -3,12 +3,13 @@ int main()
 {
     int m,n,p,q,r,i,j;
     scanf("%d" ,&m);
-    scanf("%d" ,n);
+    scanf("%d" ,&n);
+    int O[m][n];
     for(i=0;i<m;i++)
     {
         for(j=0;j<n;j++)
         {
-
+            scanf("%d" ,&O[i][j]);
         }
     }
     return 0;
