58e4ff8da45252e695122c02def03346ae3f35b5
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-25 17:59:40 +0700

    Cube Free Numbers

--- 112_quiz2_1.c
+++ 112_quiz2_1.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-    int n;
+    int n,i,count=0,cube_num,num1=2;
     scanf("%d" ,&n);
+
+    cube_num=pow(num1,3);
+    if(n%cube_num==0)
+    {
+        printf("Not Cube Free");
+    }
+    return 0;
 }
